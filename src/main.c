5f41f9555e2bae450dd94e76bbfd8059f07eeb95
zin1985 <136662625+zin1985@users.noreply.github.com>
2026-04-17 02:25:03 +0900

    Add files via upload
    更新

--- src/main.c
+++ src/main.c
@@ -1,1 +1,11 @@
+#include <gb/gb.h>
+#include <gbdk/console.h>
+#include <stdio.h>
 
+void main(void) {
+    DISPLAY_ON;
+    printf("Hello, GBDK!\n");
+    printf("GitHub Actions build OK\n");
+    printf("Press START...\n");
+    waitpad(J_START);
+}
